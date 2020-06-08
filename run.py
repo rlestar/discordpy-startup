b61bf924e9209eb14fb4611ad52b52544dbfdd7e
rlestar <62128531+rlestar@users.noreply.github.com>
2020-06-08 22:48:15 +0900

    Add files via upload

---
+++ run.py
@@ -0,0 +1,37 @@
+###ここからプログラム###
+
+import discord #discordでBOTを使うのにこれが必ず
+import random
+
+
+client = discord.Client()
+@client.event #BOT起動時にCMDに表示される部分で無くてもよい
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message): #メッセージを受け取る関数なので必ず必要
+       if message.content == 'ブキ1':
+            with open("buki.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await client.send_message(message.channel,"あなたのブキは{}です。".format(choice)) #結果を出力
+       
+       elif message.content == 'ブキ4':
+             with open("buki.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+             textlist = data.split("\n")
+             choice1 =random.choice(textlist)
+             choice2 =random.choice(textlist)
+             choice3 =random.choice(textlist)
+             choice4 =random.choice(textlist)
+             await client.send_message(message.channel,"あなたたちのブキは\n{0}\n{1}\n{2}\n{3}\nです。".format(choice1,choice2,choice3,choice4)) #結果を出力
+
+  
+
+# この＊＊＊に自分のトークンを書き替える
+client.run('NzE4MzQ2ODEyNDEwMjk4Mzk4.XtsHpw.5zn69sy--bfy_gh45ulonUELSbY')
