4cabad124880d6db927e292426291e2f5efc4e00
ripple <thefountain1993@gmail.com>
2020-06-10 00:14:44 +0900

    Update run.py

--- run.py
+++ run.py
@@ -31,7 +31,68 @@ async def on_message(message): #メッセージを受け取る関数なので必
              choice3 =random.choice(textlist)
              choice4 =random.choice(textlist)
              await message.channel.send("あなたたちのブキは\n{0}\n{1}\n{2}\n{3}\nです。".format(choice1,choice2,choice3,choice4)) #結果を出力
-
+　　　  elif message.content == 'ヒーロー1':
+            with open("hero.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力 
+       elif message.content == 'シューター1':
+            with open("shooter.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力 
+       elif message.content == 'ブラスター1':
+            with open("blaster.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力
+       elif message.content == 'ローラー1':
+            with open("roller.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力
+       elif message.content == 'フデ1':
+            with open("brush.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力
+       elif message.content == 'チャージャー1':
+            with open("charger.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力    
+       elif message.content == 'スロッシャー1':
+            with open("shosher.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力
+       elif message.content == 'スピナー1':
+            with open("splatling.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力
+       elif message.content == 'マニューバー1':
+            with open("dualies.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力
+       elif message.content == 'シェルター1':
+            with open("brella.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choice =random.choice(textlist) 
+            await message.channel.send("あなたのブキは{}です。".format(choice)) #結果を出力    　　　
+       elif message.content =='擬似':
+            await message.channel.send('https://cdn.discordapp.com/attachments/712589650694504508/719929619624624188/gijikaku.png')
   
 
 # この＊＊＊に自分のトークンを書き替える
