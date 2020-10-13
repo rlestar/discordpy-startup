d4906a93e940820740dd7395ed9e2ebc5844ade1
ripple <thefountain1993@gmail.com>
2020-10-13 20:01:10 +0900

    Update run.py

--- run.py
+++ run.py
@@ -237,15 +237,25 @@ async def on_message(message): #メッセージを受け取る関数なので必
             if message.author.guild_permissions.administrator:
                 await message.channel.send('削除しませんでした。')
                 sw = False
-       elif message.channel.id == lot_channel_id:
-            if message.content.startswith('/p'):
-                attachment = message.attachments[0]
-                guild = client.get_guild(server)
-                await attachment.save("/temp/att.gif")
-                with open("/temp/att.gif", "rb") as img:
-                   img_emoji = img.read()
-                await message.channel.send("絵文字に設定します。")
-                await guild.create_custom_emoji(name="___",image=img_emoji)
+       elif message.content == '/mw':
+            with open("atama.txt", "r",encoding="utf-8_sig") as f: 
+              data = f.read() 
+            textlist = data.split("\n")
+            choicea =random.choice(textlist) 
+            with open("huku.txt", "r",encoding="utf-8_sig") as g: 
+              data = g.read() 
+            textlist = data.split("\n")
+            choiceb =random.choice(textlist) 
+            with open("kutu.txt", "r",encoding="utf-8_sig") as h: 
+              data = h.read() 
+            textlist = data.split("\n")
+            choicec =random.choice(textlist) 
+            embed=discord.Embed(title="このメインギアパワーがついたギアを使いましょう",color=0xf038db)
+            embed.add_field(name="アタマ", value=choicea, inline=False)
+            embed.add_field(name="フク", value=choiceb, inline=False)
+            embed.add_field(name="クツ", value=choicec, inline=False)
+            await message.channel.send(embed=embed)
+                
                      
 # この＊＊＊に自分のトークンを書き替える
 client.run(os.environ['DISCORD_BOT_TOKEN'])
